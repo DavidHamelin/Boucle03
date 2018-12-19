fb7aae6c680a89057a99572d9c6bc75ea2f6b364
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-19 13:33:09 +0100

    ex 03 boucle

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boucle03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Bonjour, je suis un message généré à l'aide d'une boucle.");
+            }
+        }
+    }
+}
